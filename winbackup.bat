604e895fc2e0d8c2af39fd14525a1b9b3424b4dd
Kenko Yoshitomi <kyoshitomi.cursor@gmail.com>
2023-08-31 01:13:29 +0900

    Add del and rename commands

--- winbackup.bat
+++ winbackup.bat
@@ -6,6 +6,11 @@ set mm=%date_tmp:~4,2%
 set dd=%date_tmp:~6,2%
 set date_tmp=
 
+del .\bak\%yyyy%%mm%%dd% /Q
 mkdir .\bak\%yyyy%%mm%%dd%
 
 robocopy . .\bak\%yyyy%%mm%%dd% /XF backup.bat /XD bak /S /UNILOG+:bak\backup.log /DCOPY:T /NJH
+
+for %%f in ("bak\%yyyy%%mm%%dd%\*.*") do (
+  rename %%f %%~nf_%mm%%dd%%%~xf
+)
